7cf17d189db8cf0cad5cb514018096eaf83aa2f1
Prince Raj <Princefebraj08@gmail.com>
2020-09-26 15:44:20 +0530

    Minor change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
